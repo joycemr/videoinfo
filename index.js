3498260b8e5919a1392be55e34f09b93234fe028
Michael Joyce <michael.joyce@illiniarts.com>
2021-11-03 20:09:48 -0500

    Working Copy

---
+++ index.js
@@ -0,0 +1,20 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+})
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log('Video durations is', metadata.format.duration);
+    });
+});
